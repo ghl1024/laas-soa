6c8899c3658e3b45f4975a6b6dd173df0e106f7d
tristan-tsl <tanshilinmail@gmail.com>
2020-11-24 15:05:47 +0800

    将token验证前移到soa项目, 不能后置, 性能差距过大, 且不符合设计逻辑

--- auth.py
+++ auth.py
@@ -1,6 +1,8 @@
 """
 全局鉴权
 """
+import time
+
 import requests
 from flask import request
 
@@ -8,6 +10,13 @@ import config
 from exception import MyServiceException
 
 SOA_TOKEN_STR = "Laas-Soa-Token"
+
+local_memory_token_record = {}
+
+
+def record_2_local_memory(token, user_id):
+    global local_memory_token_record
+    local_memory_token_record[token] = {"timestamp": (time.time()), "user_id": user_id}
 
 
 def query_token_by_remote(token):
@@ -19,6 +28,9 @@ def query_token_by_remote(token):
     result = resp.json()
     if not result or len(result) < 1:
         raise MyServiceException("请求令牌查询失败, 请重新登录")
+    for item in result:
+        record_2_local_memory(token, item["user_id"])
+        break
 
 
 def do_auth():
@@ -26,7 +38,10 @@ def do_auth():
         raise MyServiceException("未登录的请求")
     token = request.headers[SOA_TOKEN_STR]
     # url_root = request.url_root # 请求的根路径, 包含请求协议、域名、端口
-    # TODO 这里可以优化性能
+    if token in local_memory_token_record:
+        local_memory_token_record_value = local_memory_token_record[token]
+        if int(time.time()) < local_memory_token_record_value["timestamp"] - 7 * 24 * 60 * 60:
+            return
     query_token_by_remote(token)
 
 
